8a2739e0f4717b0eb2b987de466f34e8284bb7f1
rialii <alice.rizzo@student.unibz.it>
2023-10-26 23:13:49 +0200

    chore: added simple unittest :white_check_mark:

--- ProjectWithDb/src/test/java/dbTests.java
+++ ProjectWithDb/src/test/java/dbTests.java
@@ -1,9 +1,33 @@
+import com.mongodb.client.MongoClient;
+import org.bson.Document;
+import org.example.DataBaseConfigurations;
 import org.junit.Test;
-public class tests {
-    @Test
-    public void test(){
 
+import javax.xml.crypto.Data;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class dbTests {
+    @Test
+    public void testDBConnection() throws IOException {
+        MongoClient mongoClient = DataBaseConfigurations.returnMongoClient();
+        assertNotNull(mongoClient);
     }
+    @Test
+    public void searchForInsertedWord_DocumentInDB() throws IOException {
+        Document document = new Document("name", "IAmInDB");
+        assertTrue(DataBaseConfigurations.searchForDocument(document));
+    }
+
+    @Test
+    public void searchForInsertedWord_DocumentNotInDB() throws IOException {
+        Document document = new Document("name", "IAmNotInDb");
+        assertFalse(DataBaseConfigurations.searchForDocument(document));
+    }
+
+
+
 
 
 }
