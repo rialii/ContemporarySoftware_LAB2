f94a6566b7fa6bf5e8b24fc1613ed64354f204f3
rialii <alice.rizzo@student.unibz.it>
2023-10-27 17:58:03 +0200

    chore: commented wrong test

--- src/test/java/dbTests.java
+++ src/test/java/dbTests.java
@@ -25,12 +25,13 @@ public class dbTests {
         assertFalse(DataBaseConfigurations.searchForDocument(document));
     }
 
+    /*
     @Test
     public void failingTestOnPurpose() throws IOException {
         Document doc = new Document("name", "IAmNotInDb");
         assertTrue(DataBaseConfigurations.searchForDocument(doc));
     }
-
+*/
 
 
 
